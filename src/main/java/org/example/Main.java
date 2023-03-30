4388af2e2365649d8fe132f9ccb915ec71ce31eb
artchaidez <67930829+artchaidez@users.noreply.github.com>
2023-03-30 08:47:06 -0500

    Corrected documentation for m1
    Cleaned up commands; Corrected documentation and moved into README

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,11 @@
 package org.example;
 
 // TODO: Logger needs to be thread safe
-// TODO: CI/CD pipeline- selenium+docker to run in parallel on machine and on Jenkins.
+// TODO: Fix bug where tests sometimes do not run in parallel
+// TODO: Be able to see selenium videos when running tests in Jenkins
 // TODO: CI/CD pipeline- Jira, Azure/AWS
 // TODO: FileLogger
-// TODO: Docker Image (needs to be done on mac)
+// TODO: Docker Image for running all tests
 public class Main {
     public static void main(String[] args) {
         System.out.println("Printing Docker Container from an Image!");
