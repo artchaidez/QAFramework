34d5ca4f7e5b0a96b85d993bd6292cdd872085cd
artchaidez <artchaidez1995@gmail.com>
2023-04-17 14:40:32 -0500

    Jenkins file
    Added Jenkinsfile for pipeline; deleted unused xml files

--- src/test/java/dockerTests/SetUpDockerGrid.java
+++ src/test/java/dockerTests/SetUpDockerGrid.java
@@ -1,8 +1,6 @@
 package dockerTests;
 
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Ignore;
+import org.testng.annotations.*;
 
 import java.io.IOException;
 
@@ -10,22 +8,22 @@ public class SetUpDockerGrid {
 
     // TODO: Figure out how to make these commands on Jenkins
     // Runs on machine by default so this needs to be removed
-    /**
-    @BeforeTest
+/**
+    @BeforeSuite
     void StartDockerGrid() throws IOException, InterruptedException {
         // cmd /c opens command prompt
         Runtime.getRuntime().exec("cmd /c start start_dockergrid.bat");
         // Does not sleep on machine
-        Thread.sleep(15000);
+        Thread.sleep(20000);
     }
 
-    @AfterTest
+    @AfterSuite
     void StopDockerGrid() throws IOException, InterruptedException {
         Runtime.getRuntime().exec("cmd /c start stop_dockergrid.bat");
         Thread.sleep(5000);
 
         Runtime.getRuntime().exec("taskkill /f /im cmd.exe"); // closes command prompt
     }
-    */
+*/
 
 }
