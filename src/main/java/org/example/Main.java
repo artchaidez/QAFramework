026fd934fff98446d2f922783786931fd0713002
artchaidez <artchaidez1995@gmail.com>
2023-04-24 16:36:50 -0500

    TODO comments added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
 
-// TODO: CI/CD pipeline- Jira, Azure/AWS.
-// TODO: Jenkins+ Selenium Grid working; bug where it does not properly remove containers sometimes
+// TODO: Convert existing HTTPClient code to RestAssured; build API testing on RestAssured
+// TODO: Rework how Verify logs pass/fail
+// TODO: Docker Image to run testsuites
+// TODO: CI/CD pipeline- Jira, Azure/AWS
 // TODO: FileLogger
-// TODO: Docker Image
+// TODO: Remove VNC password in Selenium Grid; show all browsers running (if possible)
+// TODO: Fix bug where Jenkins+ Selenium Grid it does not properly remove containers sometimes
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Printing Docker Container from an Image!");
